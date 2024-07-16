81bbaf2bdf4c92eef505fea9bf44204c684cdfcb
Martina Gutierrez <martugutierrez166@gmail.com>
2024-07-15 21:03:50 -0300

    Creación de la clase PersonajesJson + creación de los métodos GuardarPersonajes y LeerPersonajes

--- PersonajesJson.cs
+++ PersonajesJson.cs
@@ -8,11 +8,30 @@ namespace EspacioPersonajesJS
         public void GuardarPersonajes(List<Personaje> listaPersonajes, string nombreArchivo)
         {
             string personajesJson = JsonSerializer.Serialize(listaPersonajes);
-            using (FileStream abrir = new(nombreArchivo, FileMode.Open))
+            using (FileStream abrir = new(nombreArchivo,  FileMode.OpenOrCreate))
             {
-
+                using (StreamWriter escribir = new(abrir))
+                {
+                    escribir.WriteLine(personajesJson);
+                    escribir.Close();
+                }
             };
 
+        }
+
+        public List<Personaje> LeerPersonajes(string nombreArchivo)
+        {
+            string archivoJson;
+            using(FileStream abrir = new(nombreArchivo, FileMode.Open))
+            {
+                using (StreamReader leer = new(abrir))
+                {
+                    archivoJson = leer.ReadToEnd();
+                    leer.Close();
+                }
+            }
+
+            var ListaPersonajesLeidos = JsonSerializer.Deserialize<List<Personaje>>(archivoJson); 
         }
     }
 }
