b85cc4129b32ed7cedc6d35321854e6af220c7d8
Martina Gutierrez <martugutierrez166@gmail.com>
2024-07-16 20:32:27 -0300

    Se agregó un segundo constructor a la clase Personaje

--- Personaje.cs
+++ Personaje.cs
@@ -8,6 +8,12 @@ namespace EspacioPersonaje
     {
         Dato dato;
         Caracteristica caracteristica;
+
+        public Personaje(string nombre, string apodo, string casa, string varita)
+        {
+            this.dato = new Dato(nombre, apodo, casa, varita);
+            this.caracteristica = new Caracteristica();
+        }
         public Personaje(string nombre, string apodo, string casa, string varita, int violencia, int resistencia, int discrecion)
         {
             this.dato = new Dato(nombre, apodo, casa, varita);
