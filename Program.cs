a883459c937c72b0cb2fbd8de6f432caff396a26
Martina Gutierrez <martugutierrez166@gmail.com>
2024-07-15 22:39:16 -0300

    Arreglando archivo Personaje.json

--- Program.cs
+++ Program.cs
@@ -1,9 +1,20 @@
 ﻿using System;
+using EspacioPersonaje;
 using EspacioPersonajesJS;
 
 Console.WriteLine("BIENVENIDA");
 
 Console.WriteLine("Probando traída de json personajes");
 
+List<Personaje>? PruebaDepersonajes = new();
+PersonajesJson pruebaArchivoNuevo = new();
 
+string nombreArchivo = "Personajes.json";
+
+PruebaDepersonajes = pruebaArchivoNuevo.LeerPersonajes(nombreArchivo);
+
+for (int i = 0; i < 3; i++)
+{
+    Console.WriteLine(PruebaDepersonajes[i].mostrarPersonaje());
+}
 
