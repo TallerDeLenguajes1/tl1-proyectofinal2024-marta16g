64b2b88d08f26286a76df84ee58b12b58108b352
Martina Gutierrez <martugutierrez166@gmail.com>
2024-07-17 18:14:54 -0300

    Creando una lista de varitas de personajes

--- FabricaDePersonajes.cs
+++ FabricaDePersonajes.cs
@@ -8,12 +8,46 @@ namespace EspacioFabricaDePersonajes
     {
         private List<Personaje> personajes;        
 
-        // private List<string> nombre = new List<string>
-        // {
-        //     "Cho Chang",
-        //     "Draco Malfoy",
-            
-        // };
+        private List<string> nombres = new List<string>
+        {
+            "Cho Chang",
+            "Draco Malfoy",
+            "Severus Snape",
+            "Luna Lovegood",
+            "Ginny Weasley",
+            "Sirius black",
+            "Bellatrix Lestrange",
+            "Albus Dumbledore",
+            "Seamus Finnigan",
+            "Lavender Brown",
+            "Minerva McGonagall",
+            "Lucius Malfoy",
+            "Neville Longbottom",
+            "Dolores Umbridge",
+            "George Weasley",
+            "Fred Weasley",
+            "Fleur Delacour",
+            "Nymphadora Tonks"
+        };
+
+        private List<string> varitas = new List<string>
+        {
+            "Cerezo y pelos de Veela",
+            "Pino y bigotes de trol",
+            "Roble y pelo de gato Wampus",
+            "Cedro y pelo de cola de unicornio",
+            "Acebo y Coral",
+            "Cedro y plumas de cola de ave del trueno",
+            "Cerezo y pluma de cola de ave Fénix",
+            "Arce y pelo de rougarou",
+            "Cedro y fibra de corazón de dragón",
+            "Roble y pelo de Veela",
+            "Acebo y pelo de kelpie",
+            "Arce y pelo de cola de thestral",
+            "Pino y pelo de gato Wampus"
+        };
+
+        
         public FabricaDePersonajes(int cantidadDePersonajes, string nombre, string apodo, string casa, string varita)
         {
             personajes = new List<Personaje>(); 
