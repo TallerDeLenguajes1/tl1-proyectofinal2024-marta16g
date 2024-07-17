27ff56f99c0740181d0448099ba262350ddb0df6
Martina Gutierrez <martugutierrez166@gmail.com>
2024-07-17 17:02:22 -0300

    Se agregaron los campos privados de caracteristica nuevamente + se modificaron las propiedades

--- Caracteristica.cs
+++ Caracteristica.cs
@@ -5,21 +5,35 @@ namespace EspacioCaracteristica
     public class Caracteristica
     {
         private Random rand = new();
-        // private int violencia;
-        // private int resistencia; 
-        // private int discrecion ;
-        // private int salud;
-        // private int vidas;
-        public int Violencia { get;set; }
-        public int Resistencia { get;set; }
-        public int Discrecion { get;set; }
+        private int violencia;
+        private int resistencia; 
+        private int discrecion ;
+        private int salud;
+        private int vidas;
+        public int Violencia
+        {
+            get => violencia;
+            set => violencia = value;
+        }
+
+        public int Resistencia
+        {
+            get => resistencia;
+            set => resistencia = value;
+        }
+
+        public int Discrecion
+        {
+            get => discrecion;
+            set => discrecion = value;
+        }
         public int Salud { get; } = 100;
         public int Vidas { get; } = 3;
         public Caracteristica()
         {
-            Violencia = rand.Next(1,5);
-            Resistencia = rand.Next(1,5);
-            Discrecion = rand.Next(1,5);
+            Violencia = rand.Next(1,6);
+            Resistencia = rand.Next(1,6);
+            Discrecion = rand.Next(1,6);
 
         }
         public Caracteristica(int violencia, int resistencia, int discrecion)
