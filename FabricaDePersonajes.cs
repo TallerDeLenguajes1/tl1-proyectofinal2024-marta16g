ccdabe823041dc2f604dc403186fa24f8c9f8f7b
Martina Gutierrez <martugutierrez166@gmail.com>
2024-07-17 21:38:09 -0300

    Se eliminaron indices randoms de las listas en la clase FabricaDePersonajes

--- FabricaDePersonajes.cs
+++ FabricaDePersonajes.cs
@@ -6,8 +6,6 @@ namespace EspacioFabricaDePersonajes
 {
     public class FabricaDePersonajes
     {
-
-        private Random Rand = new();
         private List<Personaje> personajes;
 
         public FabricaDePersonajes()
@@ -16,14 +14,8 @@ namespace EspacioFabricaDePersonajes
 
             for (int i = 0; i < 10; i++)
             {
-                int iNombreYApodo = Rand.Next(20);
-                int iVarita = Rand.Next(13);
-                int iCasa = Rand.Next(4);
-
                 var unPersonaje = new Personaje();
                 personajes.Add(unPersonaje);
-                
-
             }
         }
 
