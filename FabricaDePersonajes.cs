73c706593991e5aec0c66c56ba37cbf349427bff
Martina Gutierrez <martugutierrez166@gmail.com>
2024-07-17 23:43:52 -0300

    Se movieron las listas de nombres, apodos, casas y varitas de la clase Dato a la clase FabricaDePersonajes

--- FabricaDePersonajes.cs
+++ FabricaDePersonajes.cs
@@ -19,28 +19,93 @@ namespace EspacioFabricaDePersonajes
         {
             personajes.Clear();
 
-            for (int i = 0; i < 10; i++)
+            int numPersonajes = 10;
+            for (int i = 0; i < numPersonajes; i++)
             {
-                int nombreYApodoLength = 20;
-                int casaLength = 4;
-                int varitaLength = 13;
 
-                int iNombre = Rand.Next(nombreYApodoLength);
-                int iApodo = Rand.Next(nombreYApodoLength);
-                int iCasa = Rand.Next(casaLength);
-                int iVarita = Rand.Next(varitaLength);
 
-                var unPersonaje = new Personaje(iNombre, iApodo, iCasa, iVarita);
+                var unPersonaje = new Personaje();
                 personajes.Add(unPersonaje);
 
                 nombreYApodoLength--;
-                casaLength--;
                 varitaLength--;
 
             }
 
             return personajes;
         }
+
+        private List<string> nombres = new List<string>
+        {
+            "Alyssa Goldworthy",
+            "Jasper Nightshade",
+            "Elara Moonstone",
+            "Cedric Thistledown",
+            "Fiona Ravenshade",
+            "Orion Ashcroft",
+            "Lilith Hawthorne",
+            "Thaddeus Stormrider",
+            "Luna Brightwood",
+            "Felix Darkwater",
+            "Morgana Spellbound",
+            "Silas Windwalker",
+            "Ivy Greenbriar",
+            "Gideon Shadowmire",
+            "Daphne Silverleaf",
+            "Lysander Blackthorn",
+            "Penelope Starfall",
+            "Rowan Frostwood",
+            "Selene Emberheart",
+            "Cassius Ironwood"
+        };
+
+        private List<string> apodos = new List<string>
+        {
+            "Sabelotodo",
+            "Rana",
+            "Guardián",
+            "Rayo McQueen",
+            "Bomba de Azufre",
+            "Tiqui-Taca",
+            "Seda majestuosa",
+            "Caramelo de cianuro",
+            "Doble cara",
+            "Diablo en botella",
+            "Majestad",
+            "Lince",
+            "Fantasma de Canterville",
+            "Espectro del bosque",
+            "Chispa de Luz",
+            "Media Naranja",
+            "Luna llena",
+            "Catástrofe",
+            "Encanto",
+            "Corazón de Piedra"
+        };
+
+        private List<string> casas = new List<string>
+        {
+            "Gryffindor",
+            "Ravenclaw",
+            "Slytherin",
+            "Hufflepuff"
+        };
+        private List<string> varitas = new List<string>
+        {
+            "Cerezo y pelos de Veela",
+            "Pino y bigotes de trol",
+            "Roble y pelo de gato Wampus",
+            "Cedro y pelo de cola de unicornio",
+            "Acebo y Coral",
+            "Cedro y plumas de cola de ave del trueno",
+            "Cerezo y pluma de cola de ave Fénix",
+            "Arce y pelo de rougarou",
+            "Cedro y fibra de corazón de dragón",
+            "Roble y pelo de Veela",
+            "Acebo y pelo de kelpie",
+            "Arce y pelo de cola de thestral",
+            "Pino y pelo de gato Wampus"
+        };
 
         public Personaje DevolverUnPersonaje(int eleccion)
         {
