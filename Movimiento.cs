05d0e60a0b2936017afd307946698f75ef81de74
Martina Gutierrez <martugutierrez166@gmail.com>
2024-07-19 11:59:29 -0300

    Se agregó un campo Persona a la clase Movimiento

--- Movimiento.cs
+++ Movimiento.cs
@@ -7,18 +7,22 @@ namespace EspacioMovimiento
         private Posiciones posicion; 
         private string? hechizo;
         private string? descripcion;
+        private int persona;
         private uint danio;
 
         public Posiciones Posicion {get;set;}
         public string? Hechizo {get;set;}
         public string? Descripcion{get;set;}
+        public int Persona{get;set;}
+
         public uint Danio{get;set;}
 
-        public Movimiento(Posiciones posicion, string hechizo, string descripcion, uint danio)
+        public Movimiento(Posiciones posicion, string hechizo, string descripcion, int persona, uint danio)
         {
             this.posicion = posicion;
             this.hechizo = hechizo;
             this.descripcion = descripcion;
+            this.persona = persona;
             this.danio = danio;
         }
 
