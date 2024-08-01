2360a8ea9dc80f05b611e5c6c982fcde623f0ef5
Martina Gutierrez <martugutierrez166@gmail.com>
2024-08-01 12:20:08 -0300

    Se modificó los valores enteros del enum Rondas y se cambió el nombre del archivo

---
+++ Rondas.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace EspacioRondas
+{
+    public enum Rondas
+    {
+        PRIMERA,
+        SEGUNDA,
+        TERCERA,
+        CUARTA,
+        QUINTA,
+        SEXTA,
+        SÉPTIMA,
+        OCTAVA,
+        NOVENA,
+        ÚLTIMA
+    }
+}
