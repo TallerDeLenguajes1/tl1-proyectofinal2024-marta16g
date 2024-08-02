642bc2378e9f929f0c70a2a00ff505394e0c51ac
Martina Gutierrez <martugutierrez166@gmail.com>
2024-08-02 20:27:26 -0300

    Se modificó el aspecto de método InicioDeRondas

--- Duelo.cs
+++ Duelo.cs
@@ -17,16 +17,11 @@ namespace EspacioDuelo
             {
                 Rondas cuenta = (Rondas)i;
                 Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("-------------------");
                 Console.WriteLine($"{cuenta} RONDA");
                 Console.WriteLine($"{jugador.Dato.Nombre} vs {enemigo.Dato.Nombre}");
                 Console.WriteLine("-------------------");
-                Console.WriteLine("Elige tu posición");
-                Console.WriteLine("1: AGRESIVO");
-                Console.WriteLine("2: DEFENSIVO");
-                Console.WriteLine("3: FURTIVO");
-            }
-            else
-            {
+            }else{
                 throw new ArgumentOutOfRangeException(nameof(i), i, "El valor de i no pertenece al rango de enum Rondas");
             }
 
