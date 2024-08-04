68bbf6dd7cc95ff886390566ded563fa37b476a1
Martina Gutierrez <martugutierrez166@gmail.com>
2024-08-04 20:33:24 -0300

    Se creó la clase Api y su método traerApi()

--- API.cs
+++ API.cs
@@ -1,1 +1,30 @@
 using System.Text.Json;
+using Microsoft.VisualBasic;
+
+namespace EspacioApi
+{
+    public class Api
+    {
+        private static readonly HttpClient client = new();
+
+        public async void obtenerApi()
+        {
+            try
+            {
+
+                var url = "https://api.generadordni.es/v2/text/paragraphs?results=2&sentences=1&language=es";
+                HttpResponseMessage  response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+    }
+}
