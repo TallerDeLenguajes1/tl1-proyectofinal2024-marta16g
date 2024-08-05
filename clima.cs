d001f6c190bd912db1dc0fb53f1d8dee5eca36a3
Martina Gutierrez <martugutierrez166@gmail.com>
2024-08-05 11:28:51 -0300

    Se cambió la estructura de la clase por otra diferente

--- clima.cs
+++ clima.cs
@@ -4,24 +4,41 @@ namespace EspacioClima
 {
 
 
-    public class Forecast
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class Current
     {
-        public string day { get; set; }
-        public string temperature { get; set; }
-        public string wind { get; set; }
+        public string time { get; set; }
+        public int interval { get; set; }
+        public double temperature_2m { get; set; }
+        public double apparent_temperature { get; set; }
+        public int is_day { get; set; }
+        public int rain { get; set; }
+    }
+
+    public class CurrentUnits
+    {
+        public string time { get; set; }
+        public string interval { get; set; }
+        public string temperature_2m { get; set; }
+        public string apparent_temperature { get; set; }
+        public string is_day { get; set; }
+        public string rain { get; set; }
     }
 
     public class Clima
     {
-        public string temperature { get; set; }
-        public string wind { get; set; }
-        public string description { get; set; }
-        public List<Forecast> forecast { get; set; }
-        public override string ToString()
-        {
-            return $"Clima:{this.temperature}";
-        }
+        public double latitude { get; set; }
+        public double longitude { get; set; }
+        public double generationtime_ms { get; set; }
+        public int utc_offset_seconds { get; set; }
+        public string timezone { get; set; }
+        public string timezone_abbreviation { get; set; }
+        public int elevation { get; set; }
+        public CurrentUnits current_units { get; set; }
+        public Current current { get; set; }
     }
+
+
 
 
 }
