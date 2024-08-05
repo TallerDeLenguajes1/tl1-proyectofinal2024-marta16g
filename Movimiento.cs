cbc655dbb15275abcca5b74e1dc543050005cf52
Martina Gutierrez <martugutierrez166@gmail.com>
2024-08-05 18:44:52 -0300

    Se modificaron getters y setters de los campos

--- Movimiento.cs
+++ Movimiento.cs
@@ -5,26 +5,55 @@ namespace EspacioMovimiento
     public class Movimiento
     {
         private Posiciones posicion;
-        private string hechizo;
-        private string descripcion;
+        private string? hechizo;
+        private string? descripcion;
         private int persona;
         private int danio;
 
-        public Posiciones Posicion { get; set; }
-        public string Hechizo { get; set; }
-        public string Descripcion { get; set; }
-        public int Persona { get; set; }
+        public Posiciones Posicion
+        {
+            get => posicion;
+            set
+            {
+                if (!Enum.IsDefined(typeof(Posiciones), value))
+                {
+                    throw new ArgumentException("Valor no válido para el enum Posiciones.", nameof(value));
+                }
+                else
+                {
+                    posicion = value;
+                }
+            }
+        }
+        public string? Hechizo
+        {
+            get => hechizo;
+            set
+            {
+                hechizo = value ?? throw new ArgumentNullException(nameof(value), "El hechizo no puede ser nulo");
+            }
+        }
+        public string? Descripcion
+        {
+            get => descripcion;
+            set
+            {
+                hechizo = value ?? throw new ArgumentNullException(nameof(value), "El hechizo no puede ser nulo");
+            }
+        }
+        public int Persona{ get; set; }
 
         public int Danio { get; set; }
 
-        public Movimiento() {
+        public Movimiento()
+        {
             Posicion = Posiciones.Agresivo;
             Hechizo = string.Empty;
             Descripcion = string.Empty;
             Persona = 2;
             Danio = 10;
 
-         }
+        }
         public Movimiento(Posiciones posicion, string hechizo, string descripcion, int persona, int danio)
         {
             this.posicion = posicion;
@@ -41,7 +70,7 @@ namespace EspacioMovimiento
 
         public List<Movimiento> FiltrarMovimientos(List<Movimiento> movimientos, Posiciones posicion)
         {
-            return movimientos.Where(m=> m.Posicion == posicion).ToList();
+            return movimientos.Where(m => m.Posicion == posicion).ToList();
         }
 
     }
