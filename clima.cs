85590461859152df9ceb31ae5abe29f3074a6fb2
Martina Gutierrez <martugutierrez166@gmail.com>
2024-08-05 19:18:23 -0300

    Se modificaron algunos campos de la clase para que acepten valor nulo

--- clima.cs
+++ clima.cs
@@ -7,7 +7,7 @@ namespace EspacioClima
     // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
     public class Current
     {
-        public string time { get; set; }
+        public string? time { get; set; }
         public int interval { get; set; }
         public double temperature_2m { get; set; }
         public double apparent_temperature { get; set; }
@@ -19,13 +19,13 @@ namespace EspacioClima
 
     public class CurrentUnits
     {
-        public string time { get; set; }
-        public string interval { get; set; }
-        public string temperature_2m { get; set; }
-        public string apparent_temperature { get; set; }
-        public string is_day { get; set; }
-        public string rain { get; set; }
-        public string weather_code { get; set; }
+        public string? time { get; set; }
+        public string? interval { get; set; }
+        public string? temperature_2m { get; set; }
+        public string? apparent_temperature { get; set; }
+        public string? is_day { get; set; }
+        public string? rain { get; set; }
+        public string? weather_code { get; set; }
     }
 
     public class Clima
@@ -34,11 +34,11 @@ namespace EspacioClima
         public double longitude { get; set; }
         public double generationtime_ms { get; set; }
         public int utc_offset_seconds { get; set; }
-        public string timezone { get; set; }
-        public string timezone_abbreviation { get; set; }
+        public string? timezone { get; set; }
+        public string? timezone_abbreviation { get; set; }
         public double elevation { get; set; }
-        public CurrentUnits current_units { get; set; }
-        public Current current { get; set; }
+        public CurrentUnits? current_units { get; set; }
+        public Current? current { get; set; }
 
        
     }
